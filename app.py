9aec0a73c73aa21eefa3e9f80e02cbef8afc6f70
Seoulcalibur <144836634+Seoulcalibur@users.noreply.github.com>
2025-03-04 11:30:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -79,11 +79,12 @@ def display_metrics_and_table(df):
                 'font-family': 'monospace'
             })
             .set_table_styles([
-                {'selector': 'th', 'props': [('min-width', '100px'), ('max-width', '200px'), ('text-align', 'right')]},
+                {'selector': 'th', 'props': [('min-width', '100px'), ('max-width', '200px')]},
                 {'selector': 'td', 'props': [('min-width', '100px'), ('max-width', '200px')]},
                 {'selector': 'th.col_heading', 'props': [('text-align', 'right')]},
                 {'selector': 'th.row_heading', 'props': [('text-align', 'right')]},
                 {'selector': 'th.index_name', 'props': [('text-align', 'right')]},
+                {'selector': 'thead th', 'props': [('text-align', 'right')]},
                 {'selector': '', 'props': [('width', '100%')]}
             ]),
             width=1200,
